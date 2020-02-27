da9a0b4f248d97300a41398599fcaef0264df62d
jimmyng94 <37247384+jimmyng94@users.noreply.github.com>
2020-02-27 18:02:26 +0000

    Update LSM6DS3_test.cpp
    test_code Working

--- Pedometer/LSM6DS3_test.cpp
+++ Pedometer/LSM6DS3_test.cpp
@@ -4,41 +4,33 @@ reference: https://learn.sparkfun.com/tutorials/lsm6ds3-breakout-hookup-guide/al
 github: https://github.com/sparkfun/SparkFun_LSM6DS3_Arduino_Library
 */
 
-#include "SparkFunLSM6DS3.h"
-#include <wiringPiI2C.h>
+#include "LSM6DS3.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <iostream>
+
 
 using namespace std;
 
 int main()
 {
-  LSM6DS3 my_imu(); // constructor
-  SensorSettings settingsIWanted;
-
+  
+  LSM6DS3 imu(I2C_MODE, 0x6B ); // constructor
+  //SensorSettings settingsIWanted;
   /*// if more than 1 device 
   LSM6DS3 SensorOne( I2C_MODE, 0x6A );
   LSM6DS3 SensorTwo( I2C_MODE, 0x6B );
   */
-  if (my_imu.begin() != 0)
-  {
-    cout<< " IMU fail to connect"<< endl;
-    return 0;
-  }
-  else {
-     cout<< " IMU to connected!"<< endl;
-  }
-  
+  imu.begin();
+ 
   while (1)
   {
     //printf(
-    cout << "acc x: ";
-    cout<< my_imu.readFloatAccelX();
-    cout<< endl;
-    //cout << "acc y: " <<my_imu.readFloatAccelY()<< endl;
-    //cout << "acc z: " <<my_imu.readFloatAccelZ()<< endl;
+    printf("acc x: %f \n", imu.readFloatAccelX());
+    printf("acc y: %f \n", imu.readFloatAccelY());
+    printf("acc z: %f \n", imu.readFloatAccelZ());
   }
-  return (0);
-   
-    
+  return 0;
  
 }
