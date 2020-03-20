c7bcccd468946b5734d79862be64eaf8df306d31
maggwood <59927980+maggwood@users.noreply.github.com>
2020-03-19 22:57:54 -0400

    Update processing.cpp
    Added acc processing

--- heartrate/signal_processing/processing.cpp
+++ heartrate/signal_processing/processing.cpp
@@ -16,6 +16,7 @@
 #include <vector> 
 #include <cmath> 
 #include <time.h> 
+#include "LSM6DS3.h"
 #include <Fir1.h>
 
 #define ADC_PIN 3
@@ -27,8 +28,22 @@ static volatile int counter = 0;
 static volatile float bpm = 0;
 static volatile float step = 0;
 static volatile int upflag = 0;
+static volatile int time = 0;
 
 Adafruit_ADS1015 ads; 
+
+void getSTEP(void){
+	float x = readFloatAccelX();
+	float y = readFloatAccelY();
+	float z = readFloatAccelZ();
+	
+	float mag = sqrt(pow(x,2)+pow(y,2)+pow(z,2));
+	if(mag > 10){
+		step++;
+	}
+//SEND STEP TO WEB :)
+return
+}
 
 void getBPM(void){ 
   //cout << "Get"; 
@@ -37,8 +52,10 @@ void getBPM(void){
   float newVal = fir.filter(val);
   if(newVal > 10){
     if(upflag == 0){
-       bpm = fs/counter*60;
-       counter ++;
+	    if(time > 0){
+      		bpm = fs/time*60;
+	    }
+	time = 0;
     }
     upflag = 25;
   }
@@ -47,6 +64,7 @@ void getBPM(void){
       upflag--;
     }
   }
+time++;
   //SEND BPM TO WEB :)
   return; 
 }
@@ -63,12 +81,63 @@ int main (int,char**)
   
   //set up adc
   ads.begin(); 
-  ads.setGain(GAIN_SIXTEEN); 
-  
-  //Interrupt when adc gets new reading
-  ads.startComparator_SingleEnded(0, 1000); 
-  if(wiringPiISR(INT_PIN, INT_EDGE_RISING, &getBPM) > 0){ 
-    //cout << "INTERRUPT!" << endl; 
-  }
+  ads.setGain(GAIN_SIXTEEN);
+//set up acc	
+LSM6DS3Core imu(I2C_MODE, 0x6B);
+
+	if (imu.beginCore() !=0)
+	{
+		printf("Error at beginCore().\n");
+	}
+	else
+	{
+		printf("\nbeginCore() passed.\n");
+	}
+
+	uint8_t errorAccumulator =0;
+
+	uint8_t dataToWrite = 0;
+	// setup accelerometer
+	dataToWrite = 0;
+
+	dataToWrite |= LSM6DS3_ACC_GYRO_FS_XL_2g;
+	dataToWrite |= LSM6DS3_ACC_GYRO_ODR_XL_26Hz;
+
+	//Now, write the patched together data
+	errorAccumulator += imu.writeRegister(LSM6DS3_ACC_GYRO_CTRL1_XL, dataToWrite);
+
+	//Set the ODR bit
+	errorAccumulator += imu.readRegister(&dataToWrite, LSM6DS3_ACC_GYRO_CTRL4_C);
+	dataToWrite &= ~((uint8_t)LSM6DS3_ACC_GYRO_BW_SCAL_ODR_ENABLED);
+	
+	// Enable embedded functions -- ALSO clears the pdeo step count
+	errorAccumulator += imu.writeRegister(LSM6DS3_ACC_GYRO_CTRL10_C, 0x3E);
+	// Enable pedometer algorithm
+	errorAccumulator += imu.writeRegister(LSM6DS3_ACC_GYRO_TAP_CFG1, 0x40);
+	// Step Detector interrupt driven to INT1 pin
+	errorAccumulator += imu.writeRegister( LSM6DS3_ACC_GYRO_INT1_CTRL, 0x10 );
+	
+	if( errorAccumulator )
+	{
+		printf("Problem configuring the device.\n");
+	}
+	else
+	{
+		printf("Device O.K.\n");
+	}
+  	uint8_t readDataByte = 0;
+		uint16_t stepsTaken = 0;
+ while (1){  
+  	//Interrupt when adc gets new reading
+  	ads.startComparator_SingleEnded(0, 1000); 
+  	if(wiringPiISR(INT_PIN, INT_EDGE_RISING, &getBPM) > 0){ 
+    		//cout << "INTERRUPT!" << endl; 
+  	}
+
+	if(time%3 == 0){ 
+    		getSTEP(); 
+  	}	
+ }
+}
 
 
