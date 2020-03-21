466b0f84feeec2faf16bb484a4cf73b3ccaa3f5f
maggwood <59927980+maggwood@users.noreply.github.com>
2020-03-21 10:43:12 -0400

    Update coefficient_generator.py

--- heartrate/signal_processing/coefficient_generator.py
+++ heartrate/signal_processing/coefficient_generator.py
@@ -3,12 +3,12 @@ import scipy.signal as ss
 
 file = open("hr_coeff.dat","w+")
 
-fs = 50
-f1 = 10
-#f2 = 105
+fs = 120
+f1 = 15
 
-B = [f1/fs*2]#,f2/fs*2]
 
-b = ss.firwin((fs*2+1),B, pass_zero = False)#,f2/fs*2]);
+B = [f1/fs*2]
+
+b = ss.firwin((fs*2+1),B,pass_zero = False)
 
 np.savetxt("hr_coeff.dat",b,newline = '\n')
