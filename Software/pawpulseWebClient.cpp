9c4c61ee426ad1dc5d0dcd86c495afb38e1077f6
tymonherzyk <60410897+tymonherzyk@users.noreply.github.com>
2020-04-18 00:52:03 +0100

    Update pawpulseWebClient.cpp

--- Software/pawpulseWebClient.cpp
+++ Software/pawpulseWebClient.cpp
@@ -1,4 +1,4 @@
-#include "../pubnub/cpp/pubnub.hpp"
+#include "libraries/pubnub/c-core/cpp/pubnub.hpp"
 #include <stdio.h> 
 #include <string.h> 
 #include <fcntl.h>
@@ -14,44 +14,29 @@
 int bpm_publish(){
     //Instantiate pubnub(bpm)
     enum pubnub_res res;
-    char const *channel = "bpm";
+    const char *channel = "bpm";
     pubnub::context pb("pub-c-f6f91f16-a809-447a-9179-f2af45ec65f1", "sub-c-0ae8a45a-5581-11ea-b828-26d2a984a2e5");
     std::string uuid("RPI_BPM");
     //Instantiate fifo(bpm)
     int fd; 
     char bpmMsg[1024]; 
-    char * bpm_fifo = "bpm_fifo"; 
+    const char * bpm_fifo = "bpm_fifo"; 
     mkfifo(bpm_fifo, 0666);
-    if (mkfifo(bpm_fifo, 0666)<0) {
-        perror("bpm_fifo status");
-    } 
-    else {
-        perror("mkfifo error");
-    }
     while(1) {
         //Start fifo(bpm)
-        printf("Waiting for bpm writer... \n");
         fd = open(bpm_fifo, O_RDONLY); //blocking waits for fifo to be opened on the other end
         if (fd==-1) {
             perror("open error");
         }
-        else {
-            printf("bpm_fifo opened \n");
-        }
         if (read(fd, bpmMsg, sizeof(bpmMsg))==-1) {
             perror("read error");
         }
-        else {
-            printf("Message read from bpm_fifo: %s \n", bpmMsg);
-        }
         close(fd);
-        printf("bpm_fifo closed \n");
-        printf("---------------------- \n");
-        //End fif(bpm)
+        //End fifo(bpm)
         //Start publishing(bpm)
         printf("Start publishing to bpm channel \n");
         pubnub::futres fr = pb.publish(channel, bpmMsg);
-        printf("Await publish... \n");
+        printf("Await publish.to bpm channel.. \n");
         res = fr.await();
         if (res == PNR_STARTED) {
             std::cout << "await() returned unexpected: PNR_STARTED" << std::endl;
@@ -73,44 +58,29 @@ int bpm_publish(){
 int step_publish(){
     //Instantiate pubnub(step)
     enum pubnub_res res;
-    char const *channel = "step";
+    const char *channel = "step";
     pubnub::context pb_2("pub-c-f6f91f16-a809-447a-9179-f2af45ec65f1", "sub-c-0ae8a45a-5581-11ea-b828-26d2a984a2e5");
     std::string uuid("RPI_STEP");
     //Instantiate fifo(step)
     int fd; 
     char stepMsg[1024]; 
-    char * step_fifo = "step_fifo"; 
+    const char * step_fifo = "step_fifo"; 
     mkfifo(step_fifo, 0666);
-    if (mkfifo(step_fifo, 0666)<0) {
-        perror("step_fifo status");
-    } 
-    else {
-        perror("mkfifo error");
-    }
     while (1) {
         //Start fifo(step)
-        printf("Waiting for step writer... \n");
         fd = open(step_fifo, O_RDONLY); //blocking waits for fifo to be opened on the other end
         if (fd==-1) {
             perror("open error");
         }
-        else {
-            printf("step_fifo opened \n");
-        }
         if (read(fd, stepMsg, sizeof(stepMsg))==-1) {
             perror("read error");
         }
-        else {
-            printf("Message read from step_fifo: %s \n", stepMsg);
-        }
         close(fd);
-        printf("step_fifo closed \n");
-        printf("---------------------- \n");
         //End fifo(step)
         //Start publishing(step)
         printf("Start publishing to step channel \n");
         pubnub::futres fr_2 = pb_2.publish(channel, stepMsg);
-        printf("Await publish... \n");
+        printf("Await publish to step channel... \n");
         res = fr_2.await();
         if (res == PNR_STARTED) {
             std::cout << "await() returned unexpected: PNR_STARTED" << std::endl;
